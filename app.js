0c85c02c9319d575d8492bcb0350f81298fe0d4a
Ravi Raj <rj12sept@gmail.com>
2022-03-22 21:44:47 +0530

    ADDING submit secret route to submit user secret and modified secrets route to show all user secrets

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -108,17 +109,46 @@ app.get("/register", function (req, res) {
 
 app.get("/secrets", function (req, res) {
 
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if(err){
+            console.log(err);
+        } else {
+            if(foundUsers) {
+                res.render("secrets", {userWithSecrets: foundUsers});
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req, res){
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+    
+    User.findById(req.user.id, function(err, foundUser){
+        if(err) {
+            console.log(err);
+        } else {
+            foundUser.secret = submittedSecret;
+            foundUser.save(function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
 });
 
 app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/");
 });
+
+
 
 
 app.post("/register", function (req, res) {
